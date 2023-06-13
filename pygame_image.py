2af25238c1f41f28acb636ccd8efd66e3f32fd8f
c0a22028 <c0a220288a@edu.teu.ac.jp>
2023-06-13 16:06:07 +0900

    初コミット

--- pygame_image.py
+++ pygame_image.py
@@ -7,14 +7,16 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("ex01-20230613/fig/pg_bg.jpg")
-    kk_image = pg.image.load("ex01-20230613/fig/3.png")
-    kk_image = pg.transform.flip(kk_image, True, False)
+    kk_img = pg.image.load("ex01-20230613/fig/3.png")
+    kk_img = pg.transform.flip(kk_img, True, False)
+    lst = [bg_img, kk_img]
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         screen.blit(bg_img, [0, 0])
+        screen.blit(kk_img, [300, 200])
         pg.display.update()
         tmr += 1        
         clock.tick(10)
